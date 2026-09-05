a306d32040e6f0f8124568c23915e58219430668
rahulreddy0120 <rahulreddy0120@users.noreply.github.com>
2026-09-05 01:27:04 +0000

    deploy: 230f61444e6fb7d3cb58adc2664275dfc5175da5

--- _next/static/R64_UVY2ffHYxBOUprSda/_buildManifest.js
+++ _next/static/R64_UVY2ffHYxBOUprSda/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/rahul-harshu-wedding//_next/:path+",
-        "destination": "/rahul-harshu-wedding/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
